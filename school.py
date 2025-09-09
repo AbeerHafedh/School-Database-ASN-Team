e12c2c137323e77fd5aa51dd0909b537ed32eb72
nawalhaider1 <itsnawallhr@gmail.com>
2025-09-09 23:19:21 +0300

    Add Teacher / ClassTeacher

--- school.py
+++ school.py
@@ -1,7 +1,6 @@
 from sqlmodel import SQLModel, Field, create_engine, Session, select
 import json
 from typing import Optional
-from sqlmodel import Session
 from sqlalchemy import text
 
 
@@ -25,6 +24,17 @@ class Student(SQLModel, table=True):
     phone_number: str
     class_id: Optional[int] = Field(default=None, foreign_key="class.id")
 
+class Teacher(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    salary: float
+    subject_id: Optional[int] = Field(default=None, foreign_key="subject.id")
+
+class ClassTeacher(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    teacher_id: Optional[int] = Field(default=None, foreign_key="teacher.id")
+    class_id: Optional[int] = Field(default=None, foreign_key="class.id")
+
 def createDb():
     SQLModel.metadata.create_all(engine)
 createDb()
@@ -36,13 +46,81 @@ def addClass(name):
         session.commit()
         session.refresh(obj)
         print(obj)
-addClass('Abeer Hafedh')
-addClass('Sarah Mohammed')
-addClass('Nawal Haider')
-
+addClass("Class A")
+addClass("Class B")
+addClass("Class C")
 
 def getAllClasses():
     with Session(engine) as session:
         obj = session.exec(select(Class)).all()
         print(obj)
 getAllClasses()
+
+
+
+def addStudent(name: str, age: int, phone_number: str, class_id: int):
+    with Session(engine) as session:
+        obj = Student(name=name, age=age, phone_number=phone_number, class_id=class_id)
+        session.add(obj)
+        session.commit()
+        session.refresh(obj)
+        print(obj)
+
+def getAllStudents():
+    with Session(engine) as session:
+        obj = session.exec(select(Student)).all()
+        print(obj)
+
+addStudent("Abeer Hafedh", 22, "07701111111", 1)
+addStudent("Sarah Mohammed", 22, "07702222222", 2)
+addStudent("Nawal Haider", 22, "07803333333", 3)
+
+getAllStudents()
+
+
+def addTeacher(name: str, salary: float, subject_id: int):
+    with Session(engine) as session:
+        obj = Teacher(name=name, salary=salary, subject_id=subject_id)
+        session.add(obj)
+        session.commit()
+        session.refresh(obj)
+        print(obj)
+
+def addClassTeacher(teacher_id: int, class_id: int):
+    with Session(engine) as session:
+        obj = ClassTeacher(teacher_id=teacher_id, class_id=class_id)
+        session.add(obj)
+        session.commit()
+        print(obj)
+
+def getAllTeachers():
+    with Session(engine) as session:
+        obj = session.exec(select(Teacher)).all()
+        print(obj)
+
+def getAllClassTeachers():
+    with Session(engine) as session:
+        obj = session.exec(select(ClassTeacher)).all()
+        print(obj)
+
+with Session(engine) as session:
+    sub = session.exec(select(Subject).where(Subject.name == "Math")).first()
+    if not sub:
+        sub = Subject(name="Math")
+        session.add(sub)
+        session.commit()
+        session.refresh(sub)
+    subject_id = sub.id
+
+
+addTeacher("Ahmed", 300, subject_id)
+addTeacher("Rana", 500, subject_id)
+addTeacher("Ali", 600, subject_id)
+
+addClassTeacher(1, 1)
+addClassTeacher(2, 2)
+addClassTeacher(3, 3)
+
+getAllTeachers()
+getAllClassTeachers()
+
